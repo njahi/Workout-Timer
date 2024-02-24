f11ea7660358e9c146c770caffe1c599e0598337
Humphrey Gatinu <njahigatinu@gmail.com>
2024-02-24 15:43:08 +0300

    added some state to the main component tree

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { useState } from "react";
+
 function App() {
-  return;
+  const [time, setTime] = useState(formatTime(newDate()));
+  function formatTime(date) {
+    return new Intl.DateTimeFormat("en", {
+      month: "short",
+      year: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    }).format(date);
+  }
+  return (
+    <main>
+      <h1>WorkOut Timer</h1>
+    </main>
+  );
 }
 
 export default App;
