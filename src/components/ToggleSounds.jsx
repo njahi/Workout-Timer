36e005ccea3a5a33dc0724f3acd0f51d2935d440
Humphrey Gatinu <njahigatinu@gmail.com>
2024-02-28 15:42:42 +0300

    rendering the button emoji as per condition which is if the allowsound state is either set to true or false

--- src/components/ToggleSounds.jsx
+++ src/components/ToggleSounds.jsx
@@ -3,7 +3,9 @@ function ToggleSounds({ allowSound, setAllowSound }) {
   return (
     <button
       className='btn-sound'
-      onClick={() => setAllowSound((allow) => !allow)}></button>
+      onClick={() => setAllowSound((allow) => !allow)}>
+      {allowSound ? "🔈" : "🔇"}
+    </button>
   );
 }
 
