fdbb91638ce19beaf4afef8b164c497cb7fedcaa
Humphrey Gatinu <njahigatinu@gmail.com>
2024-02-28 15:41:09 +0300

    new Toggle sounds component

---
+++ src/components/ToggleSounds.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+function ToggleSounds({ allowSound, setAllowSound }) {
+  return (
+    <button
+      className='btn-sound'
+      onClick={() => setAllowSound((allow) => !allow)}>
+      {allowSound ? "🔈" : "🔇"}
+    </button>
+  );
+}
+
+export default ToggleSounds;
