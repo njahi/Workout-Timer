42d5a1198501abff4af8950e3afcf440e5c2ad18
Humphrey Gatinu <njahigatinu@gmail.com>
2024-02-29 14:14:18 +0300

    memoized the new Calculator component

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,4 +1,6 @@
+/* eslint-disable react-refresh/only-export-components */
+import { memo } from "react";
 function Calculator() {
   return <div>Calculator</div>;
 }
-export default Calculator;
+export default memo(Calculator);
