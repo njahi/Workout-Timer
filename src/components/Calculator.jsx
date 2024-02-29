242fc88e1e97db52b3d746e5ef40b755011e0b53
Humphrey Gatinu <njahigatinu@gmail.com>
2024-02-29 14:25:06 +0300

    added few more states in our calculator component to enable dynamicality of our app

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -3,6 +3,9 @@
 import { memo, useState } from "react";
 function Calculator({ workouts }) {
   const [number, setNumber] = useState(workouts.at(0).numExercises);
+  const [sets, setSets] = useState(3);
+  const [speed, setSpeed] = useState(90);
+  const [duration, setDuration] = useState(5);
   return <div>Calculator</div>;
 }
 export default memo(Calculator);
