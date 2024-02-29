d11d63774f1c7ecba9fde28e082d99f3771f8812
Humphrey Gatinu <njahigatinu@gmail.com>
2024-02-29 14:22:12 +0300

    received the workouts object prop which is actually an array of objects and used the value as the initial value of the number state in our calculator component

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react-refresh/only-export-components */
-import { memo } from "react";
-function Calculator() {
+import { memo, useState } from "react";
+function Calculator({ workouts }) {
+  const [number, setNumber] = useState(workouts.at(0).numExercises);
   return <div>Calculator</div>;
 }
 export default memo(Calculator);
