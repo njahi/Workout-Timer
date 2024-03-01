52f84633f4dddb667610674ba2b012113fd8e5df
Humphrey Gatinu <njahigatinu@gmail.com>
2024-03-01 15:59:46 +0300

    used am effect to set state which is the duration state

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,12 +1,15 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react-refresh/only-export-components */
-import { memo, useState } from "react";
+import { memo, useEffect, useState } from "react";
 function Calculator({ workouts }) {
   const [number, setNumber] = useState(workouts.at(0).numExercises);
   const [sets, setSets] = useState(3);
   const [speed, setSpeed] = useState(90);
   const [durationBreak, setDurationBreak] = useState(5);
   const [duration, setDuration] = usestate(0);
+  useEffect(function () {
+    setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak);
+  });
   return (
     <>
       <form></form>
